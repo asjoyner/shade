aaf764c0db8a2fd45d560ef3154cfa18ce60c691
Shane Liebling <shanel@gmail.com>
2016-07-13 16:24:27 -0600

    more slimming

--- util.go
+++ util.go
@@ -10,14 +10,14 @@ import (
 // ConfigDir identifies the correct path to store persistent configuration data
 // on various operating systems.
 func ConfigDir() string {
-	dir := "."
 	switch runtime.GOOS {
 	case "darwin":
-		dir = path.Join(os.Getenv("HOME"), "Library", "Application Support", "shade")
+		return path.Join(os.Getenv("HOME"), "Library", "Application Support", "shade")
 	case "linux", "freebsd":
-		dir = path.Join(os.Getenv("HOME"), ".shade")
+		return path.Join(os.Getenv("HOME"), ".shade")
 	default:
+		// TODO(shanel): Should this be log instead of fmt?
 		fmt.Printf("TODO: ConfigDir on GOOS %q", runtime.GOOS)
+		return "."
 	}
-	return dir
 }
