d86383d2febe540b150c65a7da574f3d82d55979
Aaron S. Joyner <aaron@joyner.ws>
2016-07-15 09:16:41 -0400

    Fuse file tests, To/FromJSON on File, Sum methods
    This starts towards making the fuse test useful, it just populates the
    memory client with files so far.  It still needs better timing
    coordination, and to check the fuse files.  Manual testing indicates
    there's some other bug to fix, as well.
    
    I hung some helper methods off of the File struct to marshal and
    unmarshal them.  I'm not confident that this will be useful long term,
    so I haven't converted the rest of the codebase to use it yet.
    
    I am pretty confident that we want to centralize Sum, but possibly
    through methods on Chunk and File?
    
    I added a lot of comments to File and Chunk while in there.  The AES
    documentation is a bit speculative, and I have an inkling that perhaps
    a level of indirection will be necessary, eg.
    Encrypted: {
      Method: "AESGCM"
      Key:    ""
    }
    ... we'll see how that goes.

--- file.go
+++ file.go
@@ -1,19 +1,37 @@
 package shade
 
 import (
+	"encoding/json"
 	"fmt"
 	"time"
 )
 
+// File represents the metadata of a file stored in Shade.  It is stored and
+// retrieved by the drive.Client API, and boiled down
 type File struct {
-	Filename     string
-	Filesize     int64
+	// Filename is a fully qualified path, with no leading slash.
+	Filename string
+	Filesize int64 // Bytes
+
+	// ModifiedTime represents the "commit" time of this File object.  A given
+	// Filename is represented by the valid File with the latest ModifiedTime.
 	ModifiedTime time.Time
-	Chunksize    int
-	Chunks       []Chunk
-	AesKey       []byte
+
+	// Chunks represets an ordered list of the bytes in the file.
+	Chunks []Chunk
+
+	// Chunksize is the size of each plaintext Chunk, in bytes.
+	Chunksize int
+
+	// AesKey is a 256 bit key used to encrypt the Chunks with AES-GCM.  If no
+	// key is provided, the blocks are not encrypted.  The GCM nonce is stored at
+	// the front of the encrypted Chunk using gcm.Seal(); use gcm.Open() to
+	// recover the Nonce when decrypting.  Nb: This increases the encrypted
+	// Chunk's size by gcm.NonceSize(), currently 12 bytes.
+	AesKey []byte `json:",omitempty"`
 }
 
+// Chunk represents the a portion of content of the File being stored.
 type Chunk struct {
 	Index  int
 	Sha256 []byte
@@ -36,6 +54,24 @@ func (f *File) String() string {
 		}
 	}
 	return out
+}
+
+// ToJSON returns a JSON representation of the File struct.
+func (f *File) ToJSON() ([]byte, error) {
+	fj, err := json.Marshal(f)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal file %x: %s", f.Filename, err)
+	}
+	return fj, nil
+}
+
+// FromJSON populates the fields of this File struct from a JSON representation.
+// It primarily provides a convenient error message if this fails.
+func (f *File) FromJSON(fj []byte) error {
+	if err := json.Unmarshal(fj, f); err != nil {
+		return fmt.Errorf("failed to unmarshal sha256sum %x: %s", SumString(fj), err)
+	}
+	return nil
 }
 
 func (c *Chunk) String() string {
--- fusefs/fuse_test.go
+++ fusefs/fuse_test.go
@@ -1,12 +1,16 @@
 package fusefs
 
 import (
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"testing"
 	"time"
 
+	"github.com/asjoyner/shade"
 	"github.com/asjoyner/shade/cache"
 	"github.com/asjoyner/shade/drive"
 	"github.com/asjoyner/shade/drive/memory"
@@ -16,53 +20,120 @@ import (
 
 func TestFuseRead(t *testing.T) {
 	mountPoint, err := ioutil.TempDir("", "fusefsTest")
+	fmt.Println("testing at: ", mountPoint)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatalf("could not acquire TempDir: %s", err)
 	}
-	defer tearDown(mountPoint)
+	defer tearDownDir(mountPoint)
 
+	client, err := setupFuse(t, mountPoint)
+	if err != nil {
+		t.Fatalf("could not mount fuse: %s", err)
+	}
+	defer tearDownFuse(t, mountPoint)
+
+	chunkSize := 100 * 256
+
+	// Generate some random file contents
+	testChunks := make(map[string][]byte, 10)
+	for _ = range testChunks {
+		n := make([]byte, chunkSize)
+		rand.Read(n)
+		s := sha256.Sum256(n)
+		chunkSum := string(s[:])
+		testChunks[chunkSum] = n
+	}
+
+	// Populate test chunks into the client
+	for stringSum, chunk := range testChunks {
+		err := client.PutChunk([]byte(stringSum), chunk)
+		if err != nil {
+			t.Fatalf("Failed to put chunk \"%x\": %s", stringSum, err)
+		}
+	}
+
+	// Populate files, referencing those chunks, into the client
+	// the directory scheme is to break apart the shasum into dirs:
+	// deadbeef == de/ad/be/ff/deadbeef
+	// .. unless it starts with 0, so we exercise files at / too.
+	for chunkStringSum := range testChunks {
+		chs := hex.EncodeToString([]byte(chunkStringSum))
+		var filename string
+		if chs[0] == 0 {
+			filename = chs
+		} else {
+			for i := 0; i >= len(chs); i += 2 {
+				filename = fmt.Sprintf("%s/%s", filename, chs[i:2])
+			}
+		}
+		file := shade.File{
+			Filename: "",
+			Filesize: int64(len(chunkStringSum)),
+			Chunks: []shade.Chunk{{
+				Index:  0,
+				Sha256: []byte(chunkStringSum),
+			}},
+			Chunksize: chunkSize,
+		}
+		fj, err := file.ToJSON()
+		if err != nil {
+			t.Fatalf("test data is broken, could not marshal File: %s", err)
+		}
+		fileSum := sha256.Sum256(fj)
+		client.PutFile(fileSum[:], fj)
+	}
+	time.Sleep(1)
+	// TODO(asjoyner): test that the files are visible via fuse
+}
+
+// setup returns the absolute path to a mountpoint for a fuse FS, and the
+// memory-backed drive.Client which it is following.  The Client is configured
+// to refresh once per second, tests should sleep after PutFile / PutChunk.
+// TODO(asjoyner): refresh the cache on demand
+func setupFuse(t *testing.T, mountPoint string) (drive.Client, error) {
 	options := []fuse.MountOption{
 		fuse.FSName("Shade"),
-		fuse.AllowOther(),
 		fuse.NoAppleDouble(),
 	}
 	conn, err := fuse.Mount(mountPoint, options...)
 	if err != nil {
 		// often means the mountpoint is busy... but it's a TempDir...
-		t.Fatalf("Could not setup Fuse mount: %s", err)
+		return nil, fmt.Errorf("could not setup Fuse mount: %s", err)
 	}
 
 	mc, err := memory.NewClient(drive.Config{Provider: "memory"})
 	if err != nil {
-		t.Fatalf("NewClient() for test config failed: %s", err)
+		return nil, fmt.Errorf("NewClient() for test config failed: %s", err)
 	}
 
 	refresh := time.NewTicker(1 * time.Second)
 	r, err := cache.NewReader([]drive.Client{mc}, refresh)
 	if err != nil {
-		t.Fatalf("could not initialize cache: %s", err)
+		return nil, fmt.Errorf("could not initialize cache: %s", err)
 	}
 
 	ffs := New(r, conn)
 	go func() {
 		err = ffs.Serve()
 		if err != nil {
-			t.Fatalf("fuse server initialization failed: %s", err)
+			t.Errorf("fuse server initialization failed: %s", err)
 		}
 	}()
 
 	<-conn.Ready // returns when the FS is usable
 	if conn.MountError != nil {
-		t.Fatalf("fuse exited with an error: %s", err)
+		return nil, fmt.Errorf("fuse exited with an error: %s", err)
 	}
-
-	if err := fuse.Unmount(mountPoint); err != nil {
-		t.Fatalf("could not Unmount test dir: %s", err)
-	}
-
+	return mc, nil
 }
 
-func tearDown(dir string) {
+func tearDownFuse(t *testing.T, dir string) {
+	if err := fuse.Unmount(dir); err != nil {
+		t.Fatalf("could not Unmount test dir: %s", err)
+	}
+}
+
+func tearDownDir(dir string) {
 	if err := os.RemoveAll(dir); err != nil {
 		fmt.Printf("Could not clean up: %s", err)
 	}
