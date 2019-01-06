21084ddf0937ef37760b4a78bfaed7e123e6b925
Aaron S. Joyner <aaron@joyner.ws>
2019-01-06 16:08:29 -0500

    Cleanup initilization of clients, fix syntax errs

--- drive/google/remote_test.go
+++ drive/google/remote_test.go
@@ -4,8 +4,6 @@ package google
 
 import (
 	"flag"
-	"fmt"
-	"log"
 	"testing"
 
 	"github.com/asjoyner/shade/config"
@@ -16,47 +14,34 @@ var (
 	configPath = flag.String("googleTestConfig", "testdata/config.json", "Path to the config to do remote integration testing.")
 )
 
-func newTestClient() (drive.Client, err) {
+func newTestClient(t *testing.T) drive.Client {
 	cfg, err := config.Read(*configPath)
 	if err != nil {
-		return nil, fmt.Errorf("could not read remote test config from %s: %s", *configPath, err)
+		t.Fatalf("could not read remote test config from %s: %s", *configPath, err)
 	}
 	client, err := drive.NewClient(cfg)
 	if err != nil {
-		return nil, fmt.Errorf("could not initialize test client: %s\n", err)
+		t.Fatalf("could not initialize test client: %s\n", err)
 	}
-	return client, nil
+	return client
 }
 
 func TestFileRoundTrip(t *testing.T) {
-	client, err := newTestClient()
-	if err != nil {
-		log.Fatalf(err)
-	}
+	client := newTestClient(t)
 	drive.TestFileRoundTrip(t, client, 10)
 }
 
 func TestChunkRoundTrip(t *testing.T) {
-	client, err := newTestClient()
-	if err != nil {
-		log.Fatalf(err)
-	}
+	client := newTestClient(t)
 	drive.TestChunkRoundTrip(t, client, 10)
 }
 
 func TestParallelRoundTrip(t *testing.T) {
-	client, err := newTestClient()
-	if err != nil {
-		log.Fatalf(err)
-	}
+	client := newTestClient(t)
 	drive.TestParallelRoundTrip(t, client, 10)
 }
 
 func TestChunkLister(t *testing.T) {
-	client, err := newTestClient()
-	if err != nil {
-		log.Fatalf(err)
-	}
-	cfg, err := config.Read(*configPath)
+	client := newTestClient(t)
 	drive.TestChunkLister(t, client, 10)
 }
