asjoyner/shade

commit c216e9ecb2709f2a14ec23949cbc5154476dd078

Make cache implementation more deterministic
The buffered channel and goroutines for refreshes were causing
non-deterministic behavior; when Gets queued lots of refreshes, they
could complete after a Release of the same chunk.  Since refreshes
already only write to Local() clients, I decided its better to be
deterministic and just eat the latency on Get().  I optimized the
refresh so it didn't write back to the same client and ripped out the
goroutines and channels.

This does mean we'll have to copy a chunk to memory and local disk
before it can be returned from remote storage, but we don't require a
Flush() to local disk so that's mostly just writing it to a disk buffer,
which should be acceptable latency.

drive/cache/cache.go
@@ -44,18 +44,6 @@ func NewClient(c drive.Config) (drive.Client, error) {
 		d.clients = append(d.clients, child)
 	}
 	glog.V(2).Infof("my final write status is: %v", d.config.Write)
-	d.files = make(chan refreshReq, 100)
-	go func(d *Drive) {
-		for r := range d.files {
-			d.refreshFile(r.sha256sum, r.content)
-		}
-	}(d)
-	d.chunks = make(chan refreshReq, 100)
-	go func(d *Drive) {
-		for r := range d.chunks {
-			d.refreshChunk(r.sha256sum, r.content, r.f)
-		}
-	}(d)
 	return d, nil
 }
 
@@ -70,8 +58,6 @@ func NewClient(c drive.Config) (drive.Client, error) {
 type Drive struct {
 	config  drive.Config
 	clients []drive.Client
-	chunks  chan refreshReq
-	files   chan refreshReq
 	debug   bool
 }
 
@@ -109,7 +95,11 @@ func (s *Drive) GetFile(sha256sum []byte) ([]byte, error) {
 			glog.V(2).Infof("File %x not found in %q: %s", sha256sum, client.GetConfig().Provider, err)
 			continue
 		}
-		s.files <- refreshReq{sha256sum: sha256sum, content: file, f: nil}
+		for _, c := range s.clients {
+			if c.Local() && c != client {
+				c.PutFile(sha256sum, file)
+			}
+		}
 		return file, nil
 	}
 	return nil, errors.New("file not found")
@@ -154,7 +144,9 @@ func (s *Drive) PutFile(sha256sum, f []byte) error {
 // No errors are returned from child clients.
 func (s *Drive) ReleaseFile(sha256sum []byte) error {
 	for _, client := range s.clients {
-		client.ReleaseFile(sha256sum)
+		if err := client.ReleaseFile(sha256sum); err != nil {
+			glog.Infof("could not ReleaseFile in %s: %s", client.GetConfig().Provider, err)
+		}
 	}
 	return nil
 }
@@ -170,7 +162,12 @@ func (s *Drive) GetChunk(sha256sum []byte, f *shade.File) ([]byte, error) {
 			glog.V(2).Infof("Chunk %x not found in %q: %s", sha256sum, client.GetConfig().Provider, err)
 			continue
 		}
-		s.chunks <- refreshReq{sha256sum: sha256sum, content: chunk, f: f}
+		for _, c := range s.clients {
+			if c.Local() && c != client {
+				glog.V(7).Infof("refreshing chunk %x", sha256sum)
+				client.PutChunk(sha256sum, chunk, f)
+			}
+		}
 		return chunk, nil
 	}
 	return nil, errors.New("chunk not found")
@@ -214,7 +211,9 @@ func (s *Drive) PutChunk(sha256sum []byte, chunk []byte, f *shade.File) error {
 // No errors are returned from child clients.
 func (s *Drive) ReleaseChunk(sha256sum []byte) error {
 	for _, client := range s.clients {
-		client.ReleaseChunk(sha256sum)
+		if err := client.ReleaseChunk(sha256sum); err != nil {
+			glog.Infof("could not ReleaseChunk in %s: %s", client.GetConfig().Provider, err)
+		}
 	}
 	return nil
 }
@@ -306,20 +305,4 @@ func (c *ChunkLister) Err() error {
 // This populates eg. memory and disk clients with files that are
 // fetched from remote clients.  Errors are logged, but not returned.
 func (s *Drive) refreshFile(sha256sum, file []byte) {
-	for _, client := range s.clients {
-		if client.Local() {
-			client.PutFile(sha256sum, file)
-		}
-	}
-}
-
-// refreshChunk calls PutChunk on each client which is Local()
-// This populates eg. memory and disk clients with chunks that are
-// fetched from remote clients.  Errors are logged, but not returned.
-func (s *Drive) refreshChunk(sha256sum, chunk []byte, f *shade.File) {
-	for _, client := range s.clients {
-		if client.Local() {
-			client.PutChunk(sha256sum, chunk, f)
-		}
-	}
 }

drive/cache/cache_test.go
@@ -2,7 +2,6 @@ package cache
 
 import (
 	"testing"
-	"time"
 
 	"github.com/asjoyner/shade/drive"
 
@@ -52,9 +51,6 @@ func TestTwoMemoryClients(t *testing.T) {
 	cacheClient := cc.(*Drive)
 	client0 := cacheClient.clients[0].(*memory.Drive)
 	client1 := cacheClient.clients[1].(*memory.Drive)
-
-	// Give the myriad goroutines a second to coalesce
-	time.Sleep(1 * time.Second)
 
 	if err := client0.Equal(client1); err != nil {
 		t.Fatal(err)

commit a2da848f326a983b151c127b583d2b02c7d96612

Fix caching: write to the correct client variable
I broke the cache client in c216e9ecb2709f2a14ec23949cbc5154476dd078.

drive/cache/cache.go
@@ -164,9 +164,9 @@ func (s *Drive) GetChunk(sha256sum []byte, f *shade.File) ([]byte, error) {
 			continue
 		}
 		for _, c := range s.clients {
-			if c.Local() && c != client {
+			if c.Local() {
 				glog.V(7).Infof("refreshing chunk %x", sha256sum)
-				client.PutChunk(sha256sum, chunk, f)
+				c.PutChunk(sha256sum, chunk, f)
 			}
 		}
 		return chunk, nil